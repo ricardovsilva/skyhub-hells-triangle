58a32e7cd34ec5b384e46b561ec4ff07707d5e0e
Ricardo da Verdade Silva <ricardovsilva@msn.com>
2017-12-27 13:17:13 -0200

    Creates test to check if triangle is being stored

--- tests/hell_triangle_tests.py
+++ tests/hell_triangle_tests.py
@@ -13,6 +13,10 @@ class HellTriangleTests(unittest.TestCase):
     def test__init__without_pass_triangle__should_throw(self):
         assert_that(HellTriangle).raises(Exception).when_called_with()
 
+    def test__init__with_sample_triangle__should_have_sample_triangle(self):
+        target = HellTriangle(self.SAMPLE_TRIANGLE).triangle
+        assert_that(target).is_equal_to(self.SAMPLE_TRIANGLE)
+
     def test__find_maximum_total__a_triangle_of_numbers__return_the_maximum_total_from_top_to_bottom(self):
         target = HellTriangle(self.SAMPLE_TRIANGLE)
         assert_that(target.find_maximum_total()).is_equal_to(self.SAMPLE_TRIANGLE_MAXIMUM)
